30bc513e676b297dd3fa146066ea61a59d13edc1
Sam Lobel <samlobel@yahoo.com>
2016-01-18 20:10:27 -0500

    absolute click pos

--- static/drawing.js
+++ static/drawing.js
@@ -6,7 +6,6 @@ function Rect(state, x, y, w, h, fill){
 	this.w = w || 1;
 	this.h = h || 1;
 }
-
 function CanvasState(canvas){
 	
 	//simple constructor
@@ -24,21 +23,31 @@ function CanvasState(canvas){
 	this.dragoffy = 0;
 
 	this.selectionHandles = [];
+
+	this.rect = null;
+	this.upperLeft = [25,25];
+	this.bottomRight = [125,1125];
+
 	for (var i = 0; i < 8; i++) {
 		this.selectionHandles.push(new Rect(this));
 	}
+	var that = this;
 
 	canvas.addEventListener('selectstart', function(e){e.preventDefault(); return false;}, false);
 
 	canvas.addEventListener('mousedown', function(e){
+		canvasPos = $(canvas).offset();
+		// console.log(canvasPos);
 		console.log("clicked down");
-		var mx = e.clientX;
-		var my = e.clientY;
-		this.dragging = true;
-		console.log(mx, my);
+		var mx = e.pageX;
+		var my = e.pageY;
+		var relx = mx - canvasPos.left;
+		var rely = my - canvasPos.top;
+		that.dragging = true;
+		console.log(relx, rely);
 
 	}, false);
-
+	
 	canvas.addEventListener('mousemove', function(e){
 		if (this.dragging){
 			console.log("dragging");
@@ -49,11 +58,15 @@ function CanvasState(canvas){
 		this.dragging = false;
 		console.log("clicked up");
 	}, true)
-	
 
 }
 
 
+
+CanvasState.prototype.draw = function() {
+	var canvas = this.canvas;
+	canvas.clear();
+};
 
 
 
