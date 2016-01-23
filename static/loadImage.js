0f891769959177ffa133fd6b68f8d1688f5d100c
Sam Lobel <samlobel@yahoo.com>
2016-01-22 19:00:11 -0500

    works pretty darn well now. So much for intermittant commits

--- static/loadImage.js
+++ static/loadImage.js
@@ -1,4 +1,8 @@
-$(document).ready(function(){
+// $(document).ready(
+
+  window.onload =
+
+  function(){
 	console.log("loadImage function loaded!");
 	var Jcanvas = $("#drawing");
 	var drawingCanvas = $("#rectDrawing");
@@ -6,6 +10,48 @@ $(document).ready(function(){
 	// CanvasState(drawingCanvas[0]);
 	// window.CanvasState(drawingCanvas[0]);
   var that = this;
+
+  var dragger = $("#draggable");
+
+  // var stopFunctionDrag = function(e, ui){
+
+  // }
+
+  var stopFunction = function(e, ui){
+    console.log(ui);
+    var draggable = $("#draggable");
+    var editingDiv = $('#editing');
+    var canvas = document.getElementById('drawing')
+    var ctx = canvas.getContext('2d');
+    console.log(editingDiv.offset());
+    console.log(draggable.offset());
+
+    // console.log(ui.position);
+    // console.log(ui.offset);
+    // console.log(ui);
+    // console.log(draggable.width());
+    // console.log(draggable.height());
+
+    var editingOff = editingDiv.offset()
+    var editingL = editingOff.left;
+    var editingH = editingOff.top;
+    var relL = ui.position.left - editingL;
+    var relH = ui.position.top - editingH;
+    console.log(relL, relH);
+    
+    var drawW = draggable.width();
+    var drawH = draggable.height();
+    ctx.drawImage(canvas, relL, relH, drawW, drawH);
+
+
+
+  }
+  
+  // console.log("made resizable");
+  dragger.draggable({containment:'parent'});
+  dragger.resizable({containment : 'parent', minWidth:100, minHeight:100});
+  console.log("made draggable");
+
 
 	
 	function readImage(){
@@ -21,11 +67,11 @@ $(document).ready(function(){
 				console.log("and this guy too");
 				var img = new Image();
 				img.onload = function() {
-          var currentCanvas = new CanvasState(drawingCanvas[0]);
+          // var currentCanvas = new CanvasState(drawingCanvas[0]);
           setInterval(function(){
             // console.log(currentCanvas);
             // console.log(currentCanvas.draw);
-            currentCanvas.draw();
+            // currentCanvas.draw();
           }, 25);
           
           var editingDiv = $("#editing");
@@ -62,10 +108,19 @@ $(document).ready(function(){
 					var pos = Jcanvas.offset();
 					drawingCanvas.offset(pos);
 
+          var dragger = $("#draggable");
+          dragger.show();
+          dragger.offset(pos);
+          // dragger.resizable();
+          console.log("made resizable");
+          // dragger.draggable();
+          console.log("made draggable");
+
 					var canvas = Jcanvas[0];
 					var context = canvas.getContext('2d');
           console.log(drawingWidth, drawingHeight);
-					context.drawImage(img, 10, 13, drawingWidth - 20, drawingHeight - 20);
+					context.drawImage(img, 0, 0, drawingWidth, drawingHeight);
+          $("#transform").show();
 
         };
         		img.src = e.target.result;
@@ -80,8 +135,30 @@ $(document).ready(function(){
 	}
 	$('#picture').on("change", readImage);
 
+  function transform(){
+    console.log('transformed');
+    var dragger = $("#draggable");
+    var editingDiv = $("#editing");
+    var canvas = document.getElementById('drawing');
+    var ctx = canvas.getContext('2d');
+    var editingOff = editingDiv.offset();
+    var draggableOff = dragger.offset();
+    var relL = draggableOff.left - editingOff.left;
+    var relH = draggableOff.top - editingOff.top;
+    var drawW = dragger.width();
+    var drawH = dragger.height();
+    console.log(relL, relH, drawW, drawH);
 
+    for (var i =0; i < 10; i++){
+      ctx.drawImage(canvas, relL, relH, drawW, drawH);
+    }
+    dragger.hide();
+    $("#transform").hide();
+  }
 
-})
+  $("#transform").click(transform);
+
+}
+// )
 
 		
